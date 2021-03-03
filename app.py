bf14cc2a3c7bb107043f00535e87a2b76ef756d8
Yenoggy <yenoggye@yandex.ru>
2021-03-03 19:11:21 +0300

    Update

--- app.py
+++ app.py
@@ -6,13 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def master():
-    return render_template('index.html', title="Аспект", articles=articles)
+    return render_template('index.html',
+                           title="Аспект",
+                           articles=articles)
 
 @app.route('/articles/<int:article_id>')
 def get_article(article_id):
     return render_template('article.html',
                            articles=articles[article_id - 1],
                            title=articles[article_id - 1]['title'])
+
+@app.route('/auth')
+def auth():
+    return render_template('auth.html', title="Вход")
+
 
 if __name__ == '__main__':
     app.run()
