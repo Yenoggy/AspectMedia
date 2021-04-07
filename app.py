f6d8f75433c2b5d401bc2c3fa5006a1d95d6e85a
YENOGGY <yenoggye@yandex.ru>
2021-04-07 17:39:49 +0300

    Update

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 from typing import List
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from models import db, User, Article
 from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:/Study/ITMO Project/test.sqlite'
-db = SQLAlchemy(app)
+db.app = app
+db.init_app(app)
+migrate = Migrate(app, db)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    surname = db.Column(db.String(80), unique=False, nullable=False)
-    password = db.Column(db.String(120), unique=False, nullable=False)
-    admin = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.nickname
-
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(1000), unique=False, nullable=False)
-    body = db.Column(db.Text(), unique=False, nullable=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('articles', lazy=True))
-
-    def __repr__(self):
-        return f'Title: {self.title}'
 
 @app.route('/')
 def master():
---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    surname = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    admin = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.nickname
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(1000), unique=False, nullable=False)
+    body = db.Column(db.Text(), unique=False, nullable=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('articles', lazy=True))
+
+    def __repr__(self):
+        return f'Title: {self.title}'
