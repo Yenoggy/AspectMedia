571cc20e6daa875d21304bcb2a72c3404f0e1eb1
YENOGGY <yenoggye@yandex.ru>
2021-04-21 18:45:15 +0300

    Update

--- app.py
+++ app.py
@@ -4,16 +4,21 @@ from models import db, User, Article
 from flask_migrate import Migrate
 from forms import ArticleForm
 from flask_bootstrap import Bootstrap
-
+from flask_simplemde import SimpleMDE
+import json
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite'
 app.config['SECRET_KEY'] = 'AWWWWW'
+app.config['SIMPLEMDE_JS_IIFE'] = True
+app.config['SIMPLEMDE_USE_CDN'] = True
 db.app = app
 db.init_app(app)
 migrate = Migrate(app, db)
+SimpleMDE(app)
+
 
 # Главная
 @app.route('/')
@@ -23,15 +28,20 @@ def master():
                            title="Аспект",
                            articles=articles)
 
+
 # Вход
 @app.route('/auth')
 def auth():
     return render_template('auth.html', title="Вход")
 
+
 # Панель администратора
 @app.route('/admin')
 def admin_panel():
-    return render_template('admin.html', title='Панель администратора')
+    data = {'time': 1619017803724, 'blocks': [{'type': 'header', 'data': {'text': 'Главный заголовок текста', 'level': 1}}, {'type': 'paragraph', 'data': {'text': 'А это уже обычный абзац'}}, {'type': 'header', 'data': {'text': 'Не такой крутой заголовок, но тоже ничего', 'level': 3}}, {'type': 'list', 'data': {'style': 'unordered', 'items': ['Эта штука тоже огонь🔥🔥🔥', 'Звать ненумерованный список', 'Удобно что-то тут перечислять']}}, {'type': 'delimiter', 'data': {}}, {'type': 'paragraph', 'data': {'text': 'Здесь можно много чего наворотить. Предлагаю поиграться с редактором самостоятельно😏'}}, {'type': 'image', 'data': {'url': 'static/img/content.png', 'caption': 'Описание картинки', 'withBorder': True, 'withBackground': False, 'stretched': False}}], 'version': '2.20.2'}
+
+    return render_template('admin.html', title='Панель администратора', data=data)
+
 
 # ==РАБОТА С СТАТЬЯМИ==
 # Подробный просмотр статьи
@@ -42,17 +52,25 @@ def get_article(article_id):
 
 
 # Написание новой статьи
-@app.route('/articles/new', methods=('GET', 'POST') )
-def new_article():
-    form = ArticleForm()
+@app.route('/articles/new/<int:article_id>', methods=('GET', 'POST'))
+def new_article(article_id):
+    # form = ArticleForm()
+    # if request.method == 'POST':
+    #     title = form.title.data
+    #     body = form.body.data
+    #     article = Article(title=title, body=body, user=User.query.first())
+    #     db.session.add(article)
+    #     db.session.commit()
+    #     return redirect(url_for('get_article', article_id=article.id))
+    # return render_template('new_article.html', form=form)
     if request.method == 'POST':
-        title = form.title.data
-        body = form.body.data
-        article = Article(title=title, body=body, user=User.query.first())
-        db.session.add(article)
-        db.session.commit()
-        return redirect(url_for('get_article', article_id=article.id))
-    return render_template('new_article.html', form=form)
+        results = json.loads(request.data)
+        print(results)
+        return json.dumps(results)
+    with open('file.json', 'w') as f:
+        json.dump(request.form, f)
+    return render_template('new_article.html')
+
 
 # Редактирование статьи
 @app.route('/articles/<int:article_id>/edit', methods=["GET", "POST"])
@@ -71,6 +89,7 @@ def edit_article(article_id):
     form.body.data = article.body
     return render_template('edit_article.html', form=form)
 
+
 # Поиск статей
 @app.route('/search')
 def search_article():
@@ -79,6 +98,7 @@ def search_article():
     return render_template('index.html',
                            title='Аспект',
                            articles=articles)
+
 
 # Запуск приложения
 if __name__ == '__main__':
